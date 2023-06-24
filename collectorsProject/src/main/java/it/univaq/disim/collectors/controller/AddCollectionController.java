4b47d4df439f97ed93dd25449c6985eaf3e967ae
geaviozzi <geaviozzi@icloud.com>
2023-06-24 16:49:53 +0200

    piccola modifica query 6

--- collectorsProject/src/main/java/it/univaq/disim/collectors/controller/AddCollectionController.java
+++ collectorsProject/src/main/java/it/univaq/disim/collectors/controller/AddCollectionController.java
@@ -1,5 +1,54 @@
 package it.univaq.disim.collectors.controller;
 
-public class AddCollectionController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.univaq.disim.collectors.business.BusinessFactory;
+import it.univaq.disim.collectors.business.db.Query_JDBC;
+import it.univaq.disim.collectors.domain.Collection;
+import it.univaq.disim.collectors.domain.Collector;
+import it.univaq.disim.collectors.domain.Disk.State;
+import it.univaq.disim.collectors.view.ViewDispatcher;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddCollectionController implements Initializable, DataInitializable<Collector>{
+	
+	@FXML
+	private TextField nameField;
+	
+	@FXML
+	private ComboBox<State> stateComboBox;
+	
+	@FXML
+	private Button saveButton, cancelButton;
+	
+	
+	private ViewDispatcher dispatcher = ViewDispatcher.getInstance();
+	private Query_JDBC implementation = BusinessFactory.getImplementation();
+
+	private Collection collection;
+	private Collector collector;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		stateComboBox.getItems().addAll(State.values());
+		saveButton.disableProperty()
+		.bind(nameField.textProperty().isEmpty().or(Bindings.isNull(stateComboBox.valueProperty())));
+	}
+	
+	public void initializeData(Collector collector) {
+		this.collector = collector;
+		
+	}
+	@FXML
+	private void saveAction() {
+		
+	}
+
 
 }
